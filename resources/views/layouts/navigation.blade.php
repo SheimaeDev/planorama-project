109dc0cb294db213a2efb2290ae43525321ed30c
Sheimae <sheimaearn@gmail.com>
2025-05-29 21:31:10 +0200

    Responsive Nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -102,6 +102,21 @@
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div> --}}
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('events.index')" :active="request()->routeIs('events.')">
+                {{ __('Events') }}
+            </x-responsive-nav-link>
+        </div> 
+         <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('notes.index')" :active="request()->routeIs('notes.')">
+                {{ __('Notes') }}
+            </x-responsive-nav-link>
+        </div> 
+         <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('news.index')" :active="request()->routeIs('news.')">
+                {{ __('News') }}
+            </x-responsive-nav-link>
+        </div> 
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
